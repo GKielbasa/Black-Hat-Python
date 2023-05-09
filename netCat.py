ff95e5c983ac9db3a2977e3c99da0afe4ef8260e
Hizzu <hizzu@Hizzu>
2023-05-09 20:20:21 +0200

    send method

--- netCat.py
+++ netCat.py
@@ -21,10 +21,11 @@ def execute (cmd): # cmd jest poleceniem do wykoania
 # return output.decode(): Ta instrukcja zwraca wynik wykonania polecenia jako ciąg znaków. output.decode() dekoduje wynik jako tekst, ponieważ check_output zwraca bajty.
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser( #1
+    #1 tworzymy command line interface za pomoca argparse
+    parser = argparse.ArgumentParser( 
         description='BHP Net Tool',
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        #2
+        #2 wypisujemy guide dla usera jak uzywac naszego programu 
         epilog = textwrap.dedent('''Example:        
                                  netcat.py -t 192.168.1.108 -p 5555 -l -c                       # command shell
                                  netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt            # upload file
@@ -32,32 +33,59 @@ if __name__ == '__main__':
                                  echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135               # echo txt to server port 135
                                  netcat.py -t 192.168.1.108 -p 5555                             # connect to server
                                  '''))
-    
-    parser.add_argument('-c', '--command', action='store_true', help='command_shell') #3
-    parser.add_argument('-e','--execute',help='execute secified command')
-    parser.add_argument('-p', '--listen', action='store_true', help='listen')
-    parser.add_argument('-p', '--port', type=int, default=9001, help='specified port')
-    parser.add_argument('-t', '--target', default='192.168.1.1', help='specified IP')
-    parser.add_argument('-u', '--upload', help='upload file')
+    #3 dodajemy 6 argumentow ktore beda sterowac naszym programem
+    parser.add_argument('-c', '--command', action='store_true', help='command_shell') #interactive shell
+    parser.add_argument('-e','--execute',help='execute secified command')   #wykonuje jedna specificzna komende 
+    parser.add_argument('-p', '--listen', action='store_true', help='listen')   #wskazuje ze bedzie urzywany listener 
+    parser.add_argument('-p', '--port', type=int, default=9001, help='specified port')  #port do komunikacji 
+    parser.add_argument('-t', '--target', default='192.168.1.1', help='specified IP')   #target ip
+    parser.add_argument('-u', '--upload', help='upload file') #nazwa pliku do uploadu
+    #-c -e -u wspolpracuja z -l (tylko w trybie listen)
+    #-t -p wystarcza aby wskazac cel poalczenia 
     
     args = parser.parse_args()
-    if args.listen: #4
+    if args.listen: #4 kiedy korzystamy z opcji listenera tworzymy okiekt netCat z pustym bufferem 
         buffer = ''
-    else:
+    else:           # kiedy korzystamy z send do buffera idzie stdin
         buffer = sys.stdin.read()    
     nc = NetCat(args, buffer.encode())
     nc.run()
     
     
 class NetCat:
-    def __init__(self, args, buffer=None):  #1
-        self.args = args
+# w Pythonie  __init__ to konstruktor klasy self jest koniecznie wymagane aby obiekt mog sie odwolac do parametrow    
+    def __init__(self, args, buffer=None):  #1 inicjalizacja obiektu netcat wraz z argumentami z CLI i bufferem
+        self.args = args #argumenty z lini polecen przekazywane do netCata
         self.buffer = buffer
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)   #2
-        
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)   #2 tworzymy obiekt socket
+              
     def run(self):
         if self.args.listen:
-            self.listen()   #3
+            self.listen()   #3 jezeli nasluchujemy wywoluje metode listen
         else:
-            self.send() #4
+            self.send() #4 kiedy wysylamy dane wywola metode send
+            
+    def send(self):#1
+        self.socket.connect((self.args.target, self.args.port)) # podwojne nawiasy poniewaz metoda connect wymaga przekazania tupli z adresem IP i portem a owa jest definiowana przez nawiasy okr
+        if self.buffer:
+            self.socket.send(self,buffer)
+        try: #2
+            while True: #3
+                recv_len = 1
+                response = ''
+                while recv_len:
+                    data = self.socket.recv(4096)
+                    recv_len = len(data)
+                    response += data.decode()
+                    if recv_len < 4096:
+                        break   #4
+                    if response: 
+                        print(response)
+                        buffer = input('> ')
+                        buffer+= '\n'
+                        self.socket.send(buffer.encode()) #5
+        except KeyboardInterrupt: #6
+            print('User terminated.')
+            self.socket.close()
+            sys.exit()
